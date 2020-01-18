a20bd215f8842d92f52e19562d0166de4ac06c5c
Supun <hsupunw@gmail.com>
2020-01-18 23:49:53 +0530

    cors supporty

--- src/main/java/de/embl/datarest/peoplebase/repository/PersonRepository.java
+++ src/main/java/de/embl/datarest/peoplebase/repository/PersonRepository.java
@@ -3,7 +3,9 @@ package de.embl.datarest.peoplebase.repository;
 import de.embl.datarest.peoplebase.entity.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 @Repository
 public interface PersonRepository extends JpaRepository<Person, String> {
 }
